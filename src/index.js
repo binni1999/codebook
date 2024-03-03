cfe67a96b5d0c878eb8d95aa6365c75ac697c0cb
pankajbinwal001 <pankajbinwal38@gmail.com>
2024-03-03 20:30:03 +0530

    my project codebook deployment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ScrollToTop } from "./components";
+import { CartProvider, FilterProvider } from "./context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <CartProvider>
+        <FilterProvider>
+          <ScrollToTop />
+          <ToastContainer
+            closeButton={false}
+            autoClose={3000}
+            position={"bottom-right"}
+          />
+          <App />
+        </FilterProvider>
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
